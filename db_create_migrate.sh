07556e45465012b3c85a5b098812204b70fffc3c
Igor <igorbgalvan@hotmail.com>
2021-05-18 18:12:38 -0400

    database init shell scripts added

--- db_create_migrate.sh
+++ db_create_migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker exec -i api npm install yarn && yarn sequelize db:create && yarn sequelize db:migrate;
+docker exec -i api npm install -g yarn && yarn sequelize db:create && yarn sequelize db:migrate;
 exit
