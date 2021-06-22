9a56baf69213e03c2b9ea7004bd6c885ad897852
Igor <igorbgalvan@hotmail.com>
2021-06-21 22:19:57 -0400

    server configuration added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 
-require('dotenv').config({
-  path: process.env.TEST_ENV === "test" ? ".env.test" : ".env"
-});
+require('dotenv').config();
 
 require('./database');
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,8 @@
 
-require('dotenv').config({
-    path: process.env.TEST_ENV === "test" ? ".env.test" : ".env"
-  });
+require('dotenv').config();
   
 module.exports = {
-    dialect: process.env.DB_DIALECT || 'postgres',
+    dialect: 'postgres',
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
