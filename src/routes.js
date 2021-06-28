d233ced420a8a3a5ad6f75050b3f3b40f5560a17
Igor Braga <igorbgalvan@hotmail.com>
2021-06-27 22:34:59 -0400

    routes docs updated

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ const SampleController = require('./controllers/SampleController');
 const routes = express.Router();
 const path = require('path');
 
-routes.get('/docs', function (req, res) { return res.sendFile(path.join(__dirname + '/templates/example.html')); });
+routes.get('/docs', function (req, res) { return res.sendFile(path.join(__dirname + '/templates/docs.html')); });
 
 module.exports = routes;
